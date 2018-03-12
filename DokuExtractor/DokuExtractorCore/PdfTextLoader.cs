218e49b3b18752a8723411e597388a6999e59bdf
Thomas <thomas_0077@outlook.de>
2018-03-12 22:07:11 +0100

    DokuExtractorCore: PdfTextLoader (Poppler) implemented-> DokuExtractor is able to extract text from PDFs on its own. GUI: Corresponding button added

---
+++ DokuExtractor/DokuExtractorCore/PdfTextLoader.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace DokuExtractorCore
+{
+    /// <summary>
+    /// Can open a PDF file and return its content text
+    /// </summary>
+    public class PdfTextLoader
+    {
+        private bool popplerChecked;
+
+        /// <summary>
+        /// Reads the content text from a PDF file and returns it. The text layout is preserved.
+        /// </summary>
+        /// <param name="pdfFilePath">Full name of PDF file</param>
+        /// <param name="useMd5Cache">Keep extracted text files cached to improve performance if a PDF text is extracted more than one time. Reference between PDFs and text files is kept via the PDF's MD5-hash.</param>
+        /// <returns></returns>
+        public async Task<string> GetTextFromPdf(string pdfFilePath, bool useMd5Cache)
+        {
+            if (popplerChecked == false)
+                SupplyPoppler();
+
+            var pdfInfo = new FileInfo(pdfFilePath);
+            var targetFilePath = pdfFilePath + ".txt";
+
+            if (useMd5Cache)
+            {
+                var hash = CheckMD5(pdfFilePath);
+                var hashPath = Path.Combine(pdfInfo.DirectoryName, hash + ".txt");
+
+                if (File.Exists(hashPath))
+                    return File.ReadAllText(hashPath);
+                else
+                    targetFilePath = hashPath;
+            }
+
+            var pdfToTextPath = Path.Combine(Environment.CurrentDirectory, "bin", "pdftotext.exe");
+
+            var pdfProcess = new Process();
+            pdfProcess.StartInfo.FileName = pdfToTextPath;
+            pdfProcess.StartInfo.Arguments = "-layout " + pdfFilePath.EncapsulateInDoubleQuotes() + " " + targetFilePath.EncapsulateInDoubleQuotes();
+            pdfProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            var watch = new Stopwatch();
+            watch.Start();
+            pdfProcess.Start();
+            await pdfProcess.WaitForExitAsync();
+            watch.Stop();
+
+            Debug.Print("Extraction time: " + watch.Elapsed);
+
+            var retVal = File.ReadAllText(targetFilePath);
+
+            if (useMd5Cache == false)
+                File.Decrypt(targetFilePath);
+
+            return retVal;
+        }
+
+        private string CheckMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                }
+            }
+        }
+
+        private void SupplyPoppler()
+        {
+            if (File.Exists(Path.Combine(Environment.CurrentDirectory, "bin", "pdftotext.exe")) == false)
+            {
+                ZipFile.ExtractToDirectory(Path.Combine(Environment.CurrentDirectory, "poppler-0.51.zip"), Path.Combine(Environment.CurrentDirectory));
+            }
+            popplerChecked = true;
+        }
+    }
+}
--- DokuExtractor/Form1.cs
+++ DokuExtractor/Form1.cs
@@ -121,5 +121,36 @@ namespace DokuExtractorGUI
             var form = new frmTableProcessor();
             form.Show();
         }
+
+        private async void btLoadPdf_Click(object sender, EventArgs e)
+        {
+            var loader = new PdfTextLoader();
+
+            var pdfContent = await loader.GetTextFromPdf(tbInputPfad.Text, false);
+            tbInhalt.Font = new Font("Consolas", 8);
+            tbInhalt.Text = pdfContent;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            var text = string.Empty;
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (var item in files)
+            {
+                tbInputPfad.Text = item;
+            }
+
+
+        }
+
     }
 }
