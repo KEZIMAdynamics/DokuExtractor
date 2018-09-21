4e515fb5c967b28c11ce9e56d25d86b650f2680c
Gnuelpf 0815 <gnuelpf0815@outlook.de>
2018-09-21 11:17:08 +0200

    ViewerPluginPath comment for GdPicture changed

--- DokuExtractorStandardGUI/UserControls/ucViewer.cs
+++ DokuExtractorStandardGUI/UserControls/ucViewer.cs
@@ -19,8 +19,7 @@ namespace DokuExtractorStandardGUI.UserControls
 
         //TODO: Set path of used viewer plugin
         public string ViewerPluginPath { get; set; } = @"..\..\..\KezimaPdfViewer\bin\Debug\KezimaPdfViewer.dll";
-        //public string ViewerPluginPath { get; set; } = @"G:\Entwicklung\dokuextractor\KezimaPdfViewer\bin\Debug\KezimaPdfViewer.dll";
-        //public string ViewerPluginPath { get; set; } = @"G:\Entwicklung\dokuextractor\GdPicturePdfViewer\bin\Debug\GdPicturePdfViewer.dll";
+        //public string ViewerPluginPath { get; set; } = @"..\..\..\GdPicturePdfViewer\bin\Debug\GdPicturePdfViewer.dll";
 
         public delegate void TextSelectedHandler(string selectedText);
         /// <summary>
