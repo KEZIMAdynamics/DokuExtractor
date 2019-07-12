88e2ea7e33484db99516746a9972562cc69a11a1
Gnuelpf 0815 <gnuelpf0815@outlook.de>
2019-07-12 18:19:14 +0200

    DokuExtractorCore Version set to 1.3.1; DokuExtractorCore.Model Version set to 1.1.1; DokuExtractorStandardGUI Version set to 1.1.3

--- DokuExtractorStandardGUI/Properties/AssemblyInfo.cs
+++ DokuExtractorStandardGUI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DokuExtractorStandardGUI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
